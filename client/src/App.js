fd4c4bfdf89a733648dd13b820c1191fed995590
Viktor Kirillov <mr.kirillov@icloud.com>
2019-01-31 22:49:17 -0600

    Added auth services

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import config from './firebaseConfig';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isSignedIn: false,
+      userProfile: null
+    };
+  }
+
+  login(){
+    if (!this.state.isSignedIn) {
+      // Start a sign in process for an unauthenticated user.
+      var provider = new firebase.auth.GithubAuthProvider();
+      provider.addScope('repo');
+      firebase.auth().signInWithPopup(provider).then((result) => {
+        this.setState({
+          isSignedIn: true,
+          userProfile: result.user
+        });
+      });
+    }
+  }
+  
+  componentDidMount() {
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          isSignedIn: true,
+          userProfile: user
+        });
+      } else {
+        this.login();
+      }
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>

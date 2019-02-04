1dacb7ff1656667bbb30d391a490154287d17ccc
Viktor Kirillov <mr.kirillov@icloud.com>
2019-02-03 22:42:14 -0600

    Updates

---
+++ precache-manifest.02e425c11604f0d11df661af1a71e99e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "15414bd9348dbcc3c49a523f1a39f2d9",
+    "url": "/autocommit/static/media/steroidtocat.15414bd9.png"
+  },
+  {
+    "revision": "c9b87b0e8b300a4f74b18de0e0fc1c61",
+    "url": "/autocommit/static/media/orderedlistocat.c9b87b0e.png"
+  },
+  {
+    "revision": "c7ee7bd9c664681e0472e0bd9879ab34",
+    "url": "/autocommit/static/media/logo-trans.c7ee7bd9.png"
+  },
+  {
+    "revision": "c99b2b43cc115138fd13d87e847eed6e",
+    "url": "/autocommit/static/media/deckfailcat.c99b2b43.png"
+  },
+  {
+    "revision": "52328715926e387f55ef",
+    "url": "/autocommit/static/js/runtime~main.52328715.js"
+  },
+  {
+    "revision": "9264256a9ba0152ca54f",
+    "url": "/autocommit/static/js/main.9264256a.chunk.js"
+  },
+  {
+    "revision": "75e4b454844f3bf17634",
+    "url": "/autocommit/static/js/1.75e4b454.chunk.js"
+  },
+  {
+    "revision": "9264256a9ba0152ca54f",
+    "url": "/autocommit/static/css/main.69f46e8f.chunk.css"
+  },
+  {
+    "revision": "7a5e9e849601efa908c216cf693c389b",
+    "url": "/autocommit/index.html"
+  }
+];
